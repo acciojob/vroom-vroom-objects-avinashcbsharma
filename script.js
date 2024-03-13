3f8c509fa27bd0404d001eed9a43d8b46c6c0216
avinashcbsharma <156292691+avinashcbsharma@users.noreply.github.com>
2024-03-13 09:42:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
 // Complete the js code
-function Car(make, model) {}
+class Car {
+    constructor(make, model) {
+        this.make = make;
+        this.model = model;
+    }
+    getMakeModel(){
+        return `${this.make}, ${this.model}`;
+        
+    }
+}
+// let det = new Car('Mahendra','XUV300');
+// console.log(det.getMakeModel());
 
-function SportsCar(make, model, topSpeed) {}
+class SportsCar extends Car{
+    constructor(make, model, topSpeed) {
+        super (make,model);
+        this.topSpeed = parseInt(topSpeed);
+    }
+
+    getTopSpeed(){
+        return " " + this.topSpeed;
+    }
+}
+// const car = new SportsCar("Ferrari", "Testarossa", 200);
+// console.log(car.getMakeModel()); // Output: Ferrari Testarossa
+// console.log(car.getTopSpeed()); // Output: 200
 
 // Do not change the code below
 window.Car = Car;
